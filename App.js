efae470b22f6d0a0931418d321f8f2932e90cda4
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-25 15:50:36 -0300

    hello world DONE & the render method DONE

--- App.js
+++ App.js
@@ -6,4 +6,18 @@ class App extends React.Component {
   }
 }
 
+//class App extends React.Component {
+//  render() {
+//    return React.createElement('div', null, 'Hello world')
+//  }
+//}
+
+//var App = React.createClass({
+//  render: function () {
+//    return <div> Hello world </div>
+//  }
+//});
+
+//const App = () => <div>Hello world</div>
+
 export default App
