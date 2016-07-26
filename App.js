768d42d9be0e4e05d2f73686351387d78b9fe970
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-26 13:38:28 -0300

    composable components DONE

--- App.js
+++ App.js
@@ -155,38 +155,76 @@ import React from 'react';
 
 /*** 11 COMPONENT LIFECYCLE - UPDATING ***/
 
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+//
+//class App extends React.Component {
+//  constructor() {
+//    super();
+//    
+//    this.update = this.update.bind(this)
+//    this.state = { increase: false }
+//  }
+//  
+//  update() {
+//    ReactDOM.render(
+//      <App val={ this.props.val + 1 }/>, document.getElementById("app")
+//    );
+//  }
+//  
+//  componentWillReceiveProps(nextProps) {
+//    this.setState({ increase: nextProps.val > this.props.val })
+//  }
+//  
+//  shouldComponentUpdate(nextProps, nextState) {
+//    return nextProps.val % 5 === 0;
+//  }
+//  
+//  render() {
+//    console.log(this.state.increase);
+//    return (
+//      <button onClick={ this.update }>{ this.props.val }</button>
+//    )
+//  }
+//}
+//
+//App.defaultProps = { val: 0 }
 
-class App extends React.Component {
+/*** 12 HIGHER ORDER COMPONENTS ***/
+
+let Mixin = InnerComponent => class extends React.Component {
   constructor() {
     super();
     
     this.update = this.update.bind(this)
-    this.state = { increase: false }
+    this.state = { val: 0 }
   }
   
   update() {
-    ReactDOM.render(
-      <App val={ this.props.val + 1 }/>, document.getElementById("app")
-    );
-  }
-  
-  componentWillReceiveProps(nextProps) {
-    this.setState({ increase: nextProps.val > this.props.val })
-  }
-  
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
+    this.setState({ val: this.state.val + 1 })
   }
   
   render() {
-    console.log(this.state.increase);
     return (
-      <button onClick={ this.update }>{ this.props.val }</button>
+      <InnerComponent update={this.update} {...this.state} {...this.props}/>
     )
   }
 }
 
-App.defaultProps = { val: 0 }
+const Button = (props) => <button onClick={props.update}>{props.txt}: {props.val}</button>
+const Label = (props) => <label onMouseMove={props.update}>{props.txt}: {props.val}</label>
+
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <LabelMixed txt="The Black Keys"/>
+        <ButtonMixed txt="Lana Del Rey"/>
+      </div>
+    )
+  }
+}
 
 export default App
