6e94326d135d6fa532e4030c63e8b6e49b652123
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-26 15:38:33 -0300

    dinamically generated components DONE

--- App.js
+++ App.js
@@ -229,64 +229,101 @@ import React from 'react';
 
 /*** 13 COMPOSABLE COMPONENTS ***/
 
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+//
+//class App extends React.Component {
+//  constructor() {
+//    super();
+//    
+//    this.state = {
+//      red: 0,
+//    }
+//    this.update = this.update.bind(this)
+//  }
+//  
+//  update(e) {
+//    this.setState({
+//      red: ReactDOM.findDOMNode(this.refs.red.refs.slide).value,
+//    })
+//  }
+//  
+//  render() {
+//    return (
+//      <div>
+//        <Slider ref="red" type="range" min={0} max={255} step={1} val={+this.state.red} label="red" update={this.update}/>
+//      </div>
+//    );
+//  }
+//}
+//
+//class Slider extends React.Component {
+//  render() {
+//    return (
+//      <div>
+//      <input ref="slide" type={this.props.type} min={this.props.min} max={this.props.max} 
+//             step={this.props.step} defaultValue={this.props.val} onChange={this.props.update}/>
+//      {this.props.label}
+//      <br/>
+//      {this.props.val}
+//      </div>
+//    );
+//  }
+//}
+//
+//Slider.propTypes = {
+//  min: React.PropTypes.number,
+//  max: React.PropTypes.number,
+//  step: React.PropTypes.number,
+//  val: React.PropTypes.number,
+//  label: React.PropTypes.string,
+//  update: React.PropTypes.func.isRequired,
+//  type: React.PropTypes.oneOf(['range', 'number'])
+//}
+//
+//Slider.defaultProps = {
+//  min: 0,
+//  max: 255,
+//  step: 1,
+//  val: 0,
+//  label: '',
+//  type: 'range'
+//}
+
+/*** 14 DINAMICALLY GENERATED COMPONENTS ***/
 
 class App extends React.Component {
   constructor() {
     super();
     
     this.state = {
-      red: 0,
+      data: [
+        {id: 1, name: 'Bulbasaur'}, {id: 2, name: 'Ivysaur'}, {id: 3, name: 'Venusaur'},
+        {id: 4, name: 'Charmander'}, {id: 5, name: 'Charmeleon'}, {id: 6, name: 'Charizard'},
+        {id: 7, name: 'Squirtle'}, {id: 8, name: 'Wartortle'}, {id: 9, name: 'Blastoise'},
+      ]
     }
-    this.update = this.update.bind(this)
   }
   
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.slide).value,
+  render() {
+    let rows = this.state.data.map(pokemon => {
+      return <PokemonRow key={pokemon.id} data={pokemon}/>
     })
-  }
-  
-  render() {
+    
     return (
-      <div>
-        <Slider ref="red" type="range" min={0} max={255} step={1} val={+this.state.red} label="red" update={this.update}/>
-      </div>
-    );
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    )
   }
 }
 
-class Slider extends React.Component {
-  render() {
-    return (
-      <div>
-      <input ref="slide" type={this.props.type} min={this.props.min} max={this.props.max} 
-             step={this.props.step} defaultValue={this.props.val} onChange={this.props.update}/>
-      {this.props.label}
-      <br/>
-      {this.props.val}
-      </div>
-    );
-  }
-}
-
-Slider.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['range', 'number'])
-}
-
-Slider.defaultProps = {
-  min: 0,
-  max: 255,
-  step: 1,
-  val: 0,
-  label: '',
-  type: 'range'
+const PokemonRow = (props) => {
+  return (
+    <tr>
+      <td>{props.data.id}</td>
+      <td>{props.data.name}</td>
+    </tr>
+  )
 }
 
 export default App
